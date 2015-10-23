0049246c60a417ff5d6d567f40e860b24757e070
Keith Light <keith.light1986@gmail.com>
2015-10-23 12:02:40 +0100

    Switched to GULP | Changed submit function
    added GULP task runner
    changed the way the contact form will display errors with a slightly
    nicer way

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+// Include gulp
+var gulp = require('gulp');
+ // Include plugins
+var autoprefixer = require('gulp-autoprefixer');
+var cache = require('gulp-cache');
+var concat = require('gulp-concat');
+var imagemin = require('gulp-imagemin');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+
+var assets = "./build/"
+
+// Gulp Test
+gulp.task('test', function () {
+    console.log("Gulp is working! Proceed with tasks...");
+});
+
+// Clear Cache
+gulp.task('clear', function (done) {
+    console.log("Cleared cache.");
+    return cache.clearAll(done);
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    gulp.src('./js/*.js')
+      //.pipe(concat('scripts.js')) add this back in if you want concatinated js files
+        .pipe(uglify())
+        .pipe(gulp.dest(assets +'js'));
+});
+
+// Sass & Autoprefix
+gulp.task('sass', function() {
+  gulp.src('./scss/style.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefixer({
+            browsers: ["last 3 version", "> 1%", "ie 8"],
+            cascade: false
+        }))
+    .pipe(gulp.dest(assets +'css'));
+});
+
+// Images (needs to be run separately but will watch for changes)
+ gulp.task('images', function() {
+  gulp.src('./images/**/*')
+   .pipe(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true }))
+   .pipe(gulp.dest(assets +'images'));
+});
+
+// Watch
+gulp.task('watch', function() {
+   // Watch .js files
+  gulp.watch('./js/*.js', ['scripts']);
+   // Watch .scss files
+  gulp.watch('./scss/**/*', ['sass']);
+   // Watch image files
+  gulp.watch('./images/**', ['images']);
+});
+
+// Default Task
+gulp.task('default', ['scripts', 'sass', 'watch']);
