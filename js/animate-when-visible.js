206dee999f28265e144fc513588441d16aae7f01
Keith Light <keith.light1986@gmail.com>
2017-02-27 22:34:02 +0000

    Add some changes for 2017
    - Changed job, updated about section
    - Add projects, with 2 hidden as they are not yet released
    - Add PHP as an individual skill
    - Add MySQL as an additional skill
    - Amend animate when visible to check if something is already on screen
    - Add npm task to start a php server
    - Add some extra colours for the project overlays

--- js/animate-when-visible.js
+++ js/animate-when-visible.js
@@ -1,28 +1,28 @@
 (function($) {
-  var onScreenOffsetX = 0.2,
+  var jsAnimateSelector = $('.animate'),
+      onScreenOffsetX = 0.2,
       onScreenOffsetY = 0.2,
       mobOnScreenOffsetX = 0.05,
       mobOnScreenOffsetY = 0.05,
-      onScreenbreakPoint = 800;
+      onScreenBreakPoint = 800,
+      onScreen;
 
-  $(window).on('load', function() {
-    $('.animate').each(function() {
-      if($(this).isOnScreen(onScreenOffsetX, onScreenOffsetY)) {
-        $(this).removeClass('fadeInUp fadeIn');
-      }
-    });
-  });
+  if(jsAnimateSelector.length < 1) return;
 
-  $(window).on('scroll', function() {
-    $('.animate').each(function() {
-      if ($(window).width() < onScreenbreakPoint) {
+  function checkForElementsToAnimate() {
+    jsAnimateSelector.each(function() {
+      var alreadyOnScreen = $(this).offset().top < $(window).scrollTop();
+
+      if ($(window).width() < onScreenBreakPoint) {
         var on_screen = $(this).isOnScreen(mobOnScreenOffsetX, mobOnScreenOffsetY)
       } else {
         var on_screen = $(this).isOnScreen(onScreenOffsetX, onScreenOffsetY)
       }
-      if(on_screen) {
+
+      if(on_screen || alreadyOnScreen) {
         $(this).removeClass('fadeInUp fadeIn');
       }
+
       if ($('.skillz').isOnScreen(onScreenOffsetX, onScreenOffsetY)) {
         $('.js-skillz-percent').each(function() {
           $(this).css(
@@ -31,5 +31,9 @@
         });
       }
     });
+  }
+
+  $(window).on('load scroll', function() {
+    checkForElementsToAnimate();
   });
 })(jQuery);
